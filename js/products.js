1cd420cda04367ea4e11d0c5c1ba1b2f83b5d04f
MD电竞 <zhouxingzhou346@Gmail.com>
2026-07-17 08:00:35 +0800

    Update form URLs for mobile and PC platforms

--- js/products.js
+++ js/products.js
@@ -91,8 +91,8 @@ const PRODUCTS = [
     detail: '每人仅限一单，对局内出现任何问题请保存录屏联系客服处理',
     notice: '下单前请仔细阅读以下点单须知',
     platforms: [
-  { key: 'mobile', label: '手游', price: 99, unit: '单', desc: '99r保底1488w', formUrl: '你的福利单专属表单链接' },
-  { key: 'pc',     label: '端游', price: 158, unit: '单', desc: '99r保底1488w', formUrl: '你的福利单专属表单链接' },
+  { key: 'mobile', label: '手游', price: 99, unit: '单', desc: '99r保底1488w', formUrl: 'https://doc.weixin.qq.com/forms/AH0AxweMAF8Ab4AWwaDAHkCNDse3Xidhf' },
+  { key: 'pc',     label: '端游', price: 158, unit: '单', desc: '99r保底1488w', formUrl: 'https://doc.weixin.qq.com/forms/AH0AxweMAF8Ab4AWwaDAHkCN6rTC1wdbf' },
 ],
 
   },
